c20aea2fe53a7807cb01c4cc2b903eb99d99d557
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-04-30 11:15:45 +0530

    name changed

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -38,9 +38,9 @@ function Hero() {
         <h1>
           Harris
           <br />
-          Johnsen
+          Muni
         </h1>
-        <h2>Frontend Developer</h2>
+        <h2>Full-Stack Developer</h2>
         <span>
           <a href="https://twitter.com/" target="_blank">
             <img src={twitterIcon} alt="Twitter icon" />
