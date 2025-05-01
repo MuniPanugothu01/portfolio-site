86517888e94d411afd046e9312c7dfdecc136343
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-05-01 22:20:11 +0530

    hero image added here

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import styles from "./HeroStyles.module.css";
-import heroImg from "../../assets/Design uten navn.png";
+// import heroImg from "../../assets/Design uten navn.png";
+import heroImg from '../../assets/Muni-Port.png'
 import sun from "../../assets/sun.svg";
 import moon from "../../assets/moon.svg";
 import twitterLight from "../../assets/twitter-light.svg";
