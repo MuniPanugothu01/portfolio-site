e4c964a7f871c62b02aa544813fbce2d0e27a57f
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-05-01 22:34:49 +0530

    installed the bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import Footer from './sections/Footer/Footer';
 import Hero from './sections/Hero/Hero';
 import Projects from './sections/Projects/Projects';
 import Skills from './sections/Skills/Skills';
+// bootstrap cdn links
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
 
 function App() {
   return (
