ace9aff44b40ec61752a1275dc5f8bfb5f75ceaa
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-05-20 13:23:43 +0530

    added the Link Tag

--- src/Navbar/NavBar.jsx
+++ src/Navbar/NavBar.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+// impoert the Link tag
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <>
-      <link></link>
+      <Nav>
+        <Link to="/">Home</Link>
+        <Link to="/About">About-Me</Link>
+        <Link to="/Project">Projects</Link>
+        <Link to="/Skills">Skills</Link>
+        <Link to="/Contact">Contact-Me</Link>
+        <Link></Link>
+      </Nav>
     </>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
