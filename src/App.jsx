d3c3ca10fe2642798a5870a65184d3b308c7690a
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-06-08 15:25:05 +0530

    Nav bar instead

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
-import './App.css';
-import Contact from './sections/Contact/Contact';
-import Footer from './sections/Footer/Footer';
-import Hero from './sections/Hero/Hero';
-import Projects from './sections/Projects/Projects';
-import Skills from './sections/Skills/Skills';
-// bootstrap cdn links
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-
+import "./App.css";
+import Contact from "./sections/Contact/Contact";
+import Footer from "./sections/Footer/Footer";
+import Hero from "./sections/Hero/Hero";
+import Projects from "./sections/Projects/Projects";
+import Skills from "./sections/Skills/Skills";
+import NavBar from "./Navbar/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Hero />
+      {/* <Hero />
       <Projects />
       <Skills />
       <Contact />
-      <Footer />
+      <Footer /> */}
+
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/about" element={<Hero />} />
+          <Route path="/Project" element={<Projects />} />
+          <Route path="/Skills" element={<Skills />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/Navbar/NavBar.jsx
+++ src/Navbar/NavBar.jsx
@@ -1,18 +1,15 @@
 import React from "react";
-// impoert the Link tag
+// import the Link tag
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <>
-      <Nav>
-        <Link to="/">Home</Link>
-        <Link to="/About">About-Me</Link>
-        <Link to="/Project">Projects</Link>
-        <Link to="/Skills">Skills</Link>
-        <Link to="/Contact">Contact-Me</Link>
-        
-      </Nav>
+      <Link to="/">Home</Link>
+      <Link to="/About">About-Me</Link>
+      <Link to="/Project">Projects</Link>
+      <Link to="/Skills">Skills</Link>
+      <Link to="/Contact">Contact-Me</Link>
     </>
   );
 };
