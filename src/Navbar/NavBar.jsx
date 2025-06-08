1a53e70647abd498d0712532d6b2b2e6d8a0bb50
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-06-08 16:29:16 +0530

    nav bar styling

--- src/Navbar/NavBar.jsx
+++ src/Navbar/NavBar.jsx
@@ -1,16 +1,31 @@
 import React from "react";
-// import the Link tag
 import { Link } from "react-router-dom";
+import styles from "./NavBar.module.css";
+import logo from "../assets/portfolio.png";
 
 const NavBar = () => {
   return (
-    <>
-      <Link to="/">Home</Link>
-      <Link to="/About">About-Me</Link>
-      <Link to="/Project">Projects</Link>
-      <Link to="/Skills">Skills</Link>
-      <Link to="/Contact">Contact-Me</Link>
-    </>
+    <nav className={styles.navbarContainer}>
+      <Link to="/" className={styles.logoLink}>
+        <img src={logo} alt="My Logo" className={styles.logo} />
+      </Link>
+
+      <Link to="/" className={styles.navLink}>
+        Home
+      </Link>
+      <Link to="/About" className={styles.navLink}>
+        About-Me
+      </Link>
+      <Link to="/Project" className={styles.navLink}>
+        Projects
+      </Link>
+      <Link to="/Skills" className={styles.navLink}>
+        Skills
+      </Link>
+      <Link to="/Contact" className={styles.navLink}>
+        Contact-Me
+      </Link>
+    </nav>
   );
 };
 
