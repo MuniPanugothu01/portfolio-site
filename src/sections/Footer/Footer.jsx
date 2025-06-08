c7cdb5267d2470b648e07c50242a29787603e73e
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-06-08 15:46:08 +0530

    remoced the code

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -1,19 +1,3 @@
-// import styles from './FooterStyles.module.css';
-
-// function Footer() {
-//   return (
-//     <section id="footer" className={styles.container}>
-//       <p>
-//         &copy; 2025 Muni Panugothu. <br />
-//         All rights reserved. <br />
-//         <span style={{color:'red'}}>CURRENTLY : </span> Open to opportunities
-//       </p>
-//     </section>
-//   );
-// }
-
-// export default Footer;
-
 import styles from "./FooterStyles.module.css";
 import { Link } from "react-router-dom";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
