0b585f83485da3ff245eafcc351dcefd0ad7f4cf
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-06-09 14:30:45 +0530

    logo

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -7,7 +7,7 @@ import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
   return (
-    <section id="projects" className={styles.container} style={{marginTop:'-50px'}}>
+    <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
