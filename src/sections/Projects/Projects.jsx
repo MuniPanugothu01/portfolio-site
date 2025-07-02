d98cfa3f33a8f6ba09a54f0acf01dc134c6c100e
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-07-02 22:33:46 +0530

    project section completed

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,38 +1,40 @@
-import styles from './ProjectsStyles.module.css';
-import viberr from '../../assets/viberr.png';
-import freshBurger from '../../assets/LOGO_remover.png';
-import hipsster from '../../assets/hipsster.png';
-import fitLift from '../../assets/fitlift.png';
-import ProjectCard from '../../common/ProjectCard';
+import styles from "./ProjectsStyles.module.css";
+import viberr from "../../assets/viberr.png";
+import freshCart from "../../assets/LOGO_remover.png";
+import hipsster from "../../assets/hipsster.png";
+import fitLift from "../../assets/fitlift.png";
+import ProjectCard from "../../common/ProjectCard";
 
-function Projects() { 
+function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
-          src={viberr}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="Viberr"
-          p="Streaming App"
-        />
-        <ProjectCard
-          src={freshBurger}
+          src={freshCart}
           link="https://freshcart-three-eta.vercel.app/"
-          h3="Fresh Burger"
+          h3="Fresh Cart"
           p="Hamburger Restaurant"
-        />
-        <ProjectCard
-          src={hipsster}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="Hipsster"
-          p="Glasses Shop"
         />
         <ProjectCard
           src={fitLift}
           link="https://fitness-app-phi.vercel.app/"
           h3="FitLift"
           p="Fitness App"
+        />
+
+        <ProjectCard
+          src={viberr}
+          link="https://tour-planner-six.vercel.app/"
+          h3="Tourist"
+          p="Travelling Website"
+        />
+
+        <ProjectCard
+          src={hipsster}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Hipsster"
+          p="Glasses Shop"
         />
       </div>
     </section>
