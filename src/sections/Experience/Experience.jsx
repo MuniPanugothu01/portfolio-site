9e8fe17a152f622a874a61b01e25499117171708
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-07-14 12:52:04 +0530

    added the media queries

--- src/sections/Experience/Experience.jsx
+++ src/sections/Experience/Experience.jsx
@@ -1,35 +1,38 @@
 import React from "react";
-import certificateImage from '../../assets/Certificate.png'
+import certificateImage from "../../assets/Certificate.png";
 import "./Experience.css";
 
 const Experience = () => {
   return (
-    <div className="experience-card">
-      {/* Certificate Image */}
-      <img
-        src={certificateImage}
-        alt="Muni Panugothu Certificate"
-        className="certificate-image"
-      />
+    <>
+      <h1 style={{ textAlign: "center"}}>Experience</h1>
+      <div className="experience-card">
+        {/* Certificate Image */}
+        <img
+          src={certificateImage}
+          alt="Muni Panugothu Certificate"
+          className="certificate-image"
+        />
 
-      {/* Experience Content */}
-      <div className="experience-details">
-        <h2>Fullstack Developer Intern</h2>
-        <p>
-          <strong>Company:</strong> Gollamudi Technology and Software
-        </p>
-        <p>
-          <strong>Duration:</strong> Dec 2024 – May 2025 (6 Months)
-        </p>
-        <p>
-          <strong>Role:</strong> Web Developer Intern
-        </p>
-        <p>
-          <strong>Tech Stack:</strong> Vite, ReactJS, Node.js, Tailwind CSS,
-          REST APIs
-        </p>
+        {/* Experience Content */}
+        <div className="experience-details">
+          <h2>Fullstack Developer Intern</h2>
+          <p>
+            <strong>Company:</strong> Gollamudi Technology and Software
+          </p>
+          <p>
+            <strong>Duration:</strong> Dec 2024 – May 2025 (6 Months)
+          </p>
+          <p>
+            <strong>Role:</strong> Web Developer Intern
+          </p>
+          <p>
+            <strong>Tech Stack:</strong> Vite, ReactJS, Node.js, Tailwind CSS,
+            REST APIs
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
